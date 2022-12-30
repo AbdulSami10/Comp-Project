45cdb20a3546a12eb63b3bda3773d296925355ab
AbdulSami10 <sami.sayani10@gmail.com>
2022-12-30 16:39:10 +0500

    half home page is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Stack } from "@chakra-ui/react";
+import { heading, homeText } from "../content";
+import styles from "../styles/home.module.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <React.Fragment>
+      <div className={styles.main}>
+        <div className={styles.firstSection}>
+          <hi className={styles.h1}>{heading}</hi>
+          <p>{homeText}</p>
+        </div>
+        <img src="./img/mind pic.png" alt="mindPic" width="420px" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
