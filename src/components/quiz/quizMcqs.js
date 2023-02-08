88b697c938f7c6c5c43b940c3fcad31286af4e17
AbdulSami10 <sami.sayani10@gmail.com>
2023-02-08 22:35:17 +0500

    checking

---
+++ src/components/quiz/quizMcqs.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import { GiExitDoor } from "react-icons/gi";
+import styles from "../../styles/quiz.module.css";
+import { message, Popconfirm, Progress } from "antd";
+import { useNavigate } from "react-router-dom";
+import { quiz } from "../../data/content/quiz/quizContent";
+
+const QuizMcqs = (props) => {
+  const navigate = useNavigate();
+  const [queNo, setQueNo] = useState(0);
+
+  const correct = props.correct;
+  const setCorrect = props.setCorrect;
+  let percentage = props.percentage;
+  const setResult = props.setResult;
+
+  const [ans, setAns] = useState("");
+  const que = quiz[queNo];
+  const next = () => {
+    if (queNo !== quiz.length - 1) {
+      setQueNo(queNo + 1);
+    } else {
+      message.open({ type: "info", content: "Loading", duration: "0.5" });
+      setTimeout(() => {
+        setResult(true);
+      }, 500);
+    }
+
+    if (que.correct === ans) {
+      setCorrect(correct + 1);
+      console.log(correct);
+    }
+    setAns("");
+  };
+  const reset = () => {
+    setQueNo(0);
+    setCorrect(0);
+    setAns("");
+  };
+  const exitHandler = () => {
+    localStorage.removeItem("quiz");
+    setQueNo(0);
+    setCorrect(1);
+    setAns("");
+    navigate("/");
+  };
+  const cancel = () => {
+    setAns("");
+  };
+
+  return (
+    <React.Fragment>
+      <div className={styles.quizMcMain}>
+        <div className={styles.quizMcSec}>
+          <Popconfirm
+            title="Quit Game"
+            description="are sure you want to quit this game?"
+            onConfirm={exitHandler}
+          >
+            <GiExitDoor className={styles.exitIcon} />
+          </Popconfirm>
+          <p className={styles.quizMcCur}>
+            {queNo + 1} Out Of {quiz.length}
+          </p>
+        </div>
+        <h2 className={styles.quizMcTitle}>
+          {queNo + 1}) &nbsp;{que.title}
+        </h2>
+
+        <div className={styles.quizMcOptMain}>
+          <p
+            className={` ${
+              ans === "a"
+                ? `${styles.quizMcOpt} ${styles.quizMcOptActive}`
+                : styles.quizMcOpt
+            }`}
+            onClick={() => {
+              setAns("a");
+            }}
+          >
+            a)&nbsp;{que.a}
+          </p>
+          <p
+            className={` ${
+              ans === "b"
+                ? `${styles.quizMcOpt} ${styles.quizMcOptActive}`
+                : styles.quizMcOpt
+            }`}
+            onClick={() => {
+              setAns("b");
+            }}
+          >
+            b)&nbsp;{que.b}
+          </p>
+          <p
+            className={` ${
+              ans === "c"
+                ? `${styles.quizMcOpt} ${styles.quizMcOptActive}`
+                : styles.quizMcOpt
+            }`}
+            onClick={() => {
+              setAns("c");
+            }}
+          >
+            c)&nbsp;{que.c}
+          </p>
+          <p
+            className={` ${
+              ans === "d"
+                ? `${styles.quizMcOpt} ${styles.quizMcOptActive}`
+                : styles.quizMcOpt
+            }`}
+            onClick={() => {
+              setAns("d");
+            }}
+          >
+            d)&nbsp;{que.d}
+          </p>
+        </div>
+
+        <Button onClick={next} className={styles.nxtBtn}>
+          Next
+        </Button>
+        <Button className={styles.cancelBtn} onClick={cancel}>
+          Cancel
+        </Button>
+
+        <Button onClick={reset} className={styles.resetBtn}>
+          Reset
+        </Button>
+      </div>
+      <Progress percent={percentage} />
+    </React.Fragment>
+  );
+};
+
+export default QuizMcqs;
---
+++ src/components/quiz/quizResult.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  quizResBest,
+  quizResNorml,
+  quizResWorst,
+} from "../../data/content/quiz/index";
+import { quiz } from "../../data/content/quiz/quizContent";
+import styles from "../../styles/quiz.module.css";
+import Button from "../../UI/Button/Button";
+
+const QuizResult = (props) => {
+  const setResult = props.setResult;
+  const correct = props.correct;
+  let percentage = props.percentage;
+  const setCorrect = props.setCorrect;
+  const navigate = useNavigate();
+  const home = () => {
+    setCorrect(0);
+
+    setResult(false);
+    navigate("/");
+  };
+  return (
+    <React.Fragment>
+      <div className={styles.quizResMain}>
+        <div>
+          <h2>
+            {percentage <= 40
+              ? quizResWorst
+              : percentage >= 80
+              ? quizResBest
+              : percentage <= 80 || percentage >= 40
+              ? quizResNorml
+              : ""}
+          </h2>
+          <p>
+            Your {correct} out of {quiz.length}
+          </p>
+
+          <p>Your Percentage is {percentage}%</p>
+          <Button className={styles.cancelBtn} onClick={home}>
+            Back To Home
+          </Button>
+        </div>
+        <img src="./img/quizRes.webp" alt="quizResultImage" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default QuizResult;
